804703edef0e659273369d0a05c5a1b6ae0b040d
Rasmus Madsen <ram006@edu.zealand.dk>
2026-05-05 13:53:18 +0200

    Oprettede oprettelse af ingredienser

---
+++ Sablou Web/Pages/Ingredienser/Opret.cshtml.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Sablou_Web.Models;
+using Sablou_Web.Services;
+
+namespace Sablou_Web.Pages.Ingredienser
+{
+    public class OpretModel : PageModel
+    {
+        private IDataService _repositories;
+
+        [BindProperty]
+        public string Navn { get; set; }
+        [BindProperty]
+        public string Beskrivelse { get; set; }
+
+        public OpretModel()
+        {
+            _repositories = new Dataservice();
+        }
+
+        public IActionResult OnPost()
+        {
+            if(ModelState.IsValid == false)
+            {
+               return Page();
+            }
+            _repositories.IngrediensRepository.Create(new Ingrediens(Navn,Beskrivelse));
+
+            return RedirectToPage("Alle");
+        }
+    }
+}
