f026602838e6dc2600c34e6d2fa97edd5f7c7d57
Christian Allerslev Hansen <chh015@edu.zealand.dk>
2026-05-05 12:35:23 +0200

    Lavet starten på en forside

---
+++ Sablou Web/Pages/Forside.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Sablou_Web.Pages
+{
+    public class ForsideModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
