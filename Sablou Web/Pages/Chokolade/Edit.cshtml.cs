f2c480556132a2c508b3ecb838be01dab07b703c
Troels Aagaard Skalkam <troels@skalkam.dk>
2026-05-06 10:25:14 +0200

    småfejl fjernet

--- Sablou Web/Pages/Chokolade/Edit.cshtml.cs
+++ Sablou Web/Pages/Chokolade/Edit.cshtml.cs
@@ -38,8 +38,7 @@ namespace Sablou_Web.Pages.Chokolade
             return Page();
         }
 
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more information, see https://aka.ms/RazorPagesCRUD.
+
         public async Task<IActionResult> OnPostAsync()
         {
             if (!ModelState.IsValid)
