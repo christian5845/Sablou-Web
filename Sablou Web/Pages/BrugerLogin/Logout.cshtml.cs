7055c5efd49e0fa1b4c4b93369824efcfad4f82e
Søren Palle Dyrbye <sod004@edu.zealand.dk>
2026-05-07 10:23:27 +0200

    Logout implementeret

--- Sablou Web/Pages/BrugerLogin/Logout.cshtml.cs
+++ Sablou Web/Pages/BrugerLogin/Logout.cshtml.cs
@@ -1,12 +1,20 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Sablou_Web.Pages.BrugerLogin;
 
-namespace Sablou_Web.Pages.BrugerLogin
+namespace Sablou_Web.Pages.BrugerLogin;
+
+public class LogoutModel : PageModel
 {
-    public class LogoutModel : PageModel
+    public async Task<IActionResult> OnGet()
     {
-        public void OnGet()
-        {
-        }
+        LoginModel.CurrentBruger = null;
+
+        await HttpContext.SignOutAsync(
+            CookieAuthenticationDefaults.AuthenticationScheme);
+
+        return RedirectToPage("/Forside");
     }
 }
