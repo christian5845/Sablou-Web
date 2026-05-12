a43f382560cdc6435a434c4698ce1170eb4e23cf
Rasmus Madsen <ram006@edu.zealand.dk>
2026-05-12 11:12:24 +0200

    Tilføjede side til at opdatere kataloger

--- Sablou Web/Pages/Kataloger/OpdaterKatalog.cshtml.cs
+++ Sablou Web/Pages/Kataloger/OpdaterKatalog.cshtml.cs
@@ -7,14 +7,33 @@ namespace Sablou_Web.Pages.Kataloger;
 public class OpdaterKatalogModel : PageModel
 {
 
+
     public IDataService Repositories { get; }
+
+    [BindProperty]
+    public string Valg { get; set; }
+
+    public bool ErKatalogValgt { get; set; }
 
     public OpdaterKatalogModel(IDataService ds)
     {
         Repositories = ds;
     }
 
-    public void OnGet()
+    public void OnGet(int id)
     {
+        if (id == null)
+        {
+            ErKatalogValgt = false;
+        }
+        else
+        {
+            ErKatalogValgt = true;
+        }
     }
+
+    //public IActionResult OnPost()
+    //{
+
+    //}
 }
--- Sablou Web/Pages/Kataloger/SommerKatalog.cshtml.cs
+++ Sablou Web/Pages/Kataloger/SommerKatalog.cshtml.cs
@@ -1,12 +1,16 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Sablou_Web.Pages.Kataloger
+namespace Sablou_Web.Pages.Kataloger;
+
+public class SommerKatalogModel : PageModel
 {
-    public class SommerKatalogModel : PageModel
+
+
+
+    public void OnGet()
     {
-        public void OnGet()
-        {
-        }
+
+
     }
 }
