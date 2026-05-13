c83288397005e8c413e7436c9e6e788e85658301
Jonas Boye Jensen <joj008@edu.zealand.dk>
2026-05-13 12:57:36 +0200

    Slettet VoresChokolader og flyttet det op under Chokolader-mappen.

--- Sablou Web/Pages/Chokolader/VoresChokolader.cshtml.cs
+++ Sablou Web/Pages/Chokolader/VoresChokolader.cshtml.cs
@@ -1,30 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Sablou_Web.Services.Repositories;
+using Sablou_Web.Services;
+using Sablou_Web.Models;
 
-namespace Sablou_Web.Pages.Chokolader
+namespace Sablou_Web.Pages;
+
+public class VoresChokoladerModel : PageModel
 {
-    public class VoresChokoladerModel : PageModel
+    public IDataService Repositories { get; }
+
+    public VoresChokoladerModel(IDataService dataService)
     {
-        private readonly ChokoladeRepository _repository;
-
-        public VoresChokoladerModel(ChokoladeRepository repository)
-        {
-            _repository = repository;
-        }
-
-        public List<Sablou_Web.Models.Chokolade> Chokolader { get; set; } = new();
-        [BindProperty(SupportsGet = true)]
-        public string Search { get; set; } = string.Empty;
-        [BindProperty(SupportsGet = true)]
-        public string Sort { get; set; } = "name";
-        public int Page { get; set; } = 1;
-        public int TotalPages { get; set; } = 1;
-
-        public void OnGet()
-        {
-
-            Chokolader = _repository.Data.Values.ToList();
-        }
+        Repositories = dataService;
     }
+
+    public void OnGet()
+    {
+    }
+
 }
