6fc8200719bb0add03c999e4efd8ae97ccd379c1
Rasmus Madsen <ram006@edu.zealand.dk>
2026-05-15 11:13:53 +0200

    Tilføjede ny side

--- Sablou Web/Pages/Kataloger/VoresKatalog.cshtml.cs
+++ Sablou Web/Pages/Kataloger/VoresKatalog.cshtml.cs
@@ -1,12 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Sablou_Web.Pages.Kataloger
+namespace Sablou_Web.Pages.Kataloger;
+
+public class VoresKatalogModel : PageModel
 {
-    public class VoresKatalogModel : PageModel
+    public void OnGet()
     {
-        public void OnGet()
-        {
-        }
     }
 }
---
+++ Sablou Web/Pages/Kataloger/Voreshøjtid.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Sablou_Web.Pages.Kataloger
+{
+    public class VoreshøjtidModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
