594c906b6f6fe0516d3c471b8d03e87ed78a468c
Troels Aagaard Skalkam <troels@skalkam.dk>
2026-05-18 02:13:52 +0200

    lavet chokoladeprisextension for at vise priserne ordentligt, og derefter ændret i mange cshtml'er for at få prisen vist via models

---
+++ Sablou Web/Models/ChokoladePrisExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+
+namespace Sablou_Web.Models
+{
+    // Denne klasse bruges til at lave en pæn pris-tekst for chokolade
+    public static class ChokoladePrisExtensions
+    {
+        // Denne metode kan bruges direkte på en Chokolade, fx c.VisPris()
+        public static string VisPris(this Chokolade chokolade)
+        {
+            try
+            {
+                // Hvis chokoladen ikke findes, vises en fejlbesked
+                if (chokolade == null)
+                    throw new Exception("Pris ikke tilgængelig");
+
+                // Hvis prisen er 0 eller mindre, vises en fejlbesked
+                if (chokolade.Stykpris <= 0)
+                    throw new Exception("Pris ikke tilgængelig");
+
+                // Prisen vises med 2 decimaler og dansk format
+                return chokolade.Stykpris.ToString("0.00", CultureInfo.GetCultureInfo("da-DK")) + " kr.";
+            }
+            catch
+            {
+                // Hvis der sker en fejl, vises denne besked i stedet
+                return "Pris ikke tilgængelig";
+            }
+        }
+    }
+}
