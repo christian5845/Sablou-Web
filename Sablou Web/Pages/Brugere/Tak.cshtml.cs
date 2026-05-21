83aab1cf7150c60e766540fef1c2280694e54d66
Jonas Boye Jensen <joj008@edu.zealand.dk>
2026-05-21 12:54:58 +0200

    Fixet footer

---
+++ Sablou Web/Pages/Brugere/Tak.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Sablou_Web.Pages.Brugere
+{
+    public class TakModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
